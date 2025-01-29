034eb468d65acbecbbdd5cc2a19281f3265183b1
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-01-29 15:19:44 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,num3;
+  printf("\nEnter three numbers");
+  scanf("%d%d%d",&num1,&num2,&num3);
+  if (num1>num2){
+     if (num2 >num3){
+            printf("Number1 is the largest");
+     }
+     else if (num3>num2){
+            if (num1<num3) {
+                printf("number3 is largest");
+            }
+     }
+   }             
+   else{
+      if (num2 >num3){
+            printf("Number2 is the largest");
+   }
+      else if (num3>num2){
+            
+            printf("number3 is largest");
+     }
+  }
+  return 0;
+}
