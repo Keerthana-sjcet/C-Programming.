f36739cd7dab78570b374e41129893881fdbebf4
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-01-29 15:45:41 +0530

    largest.c

--- largest.c
+++ largest.c
@@ -1,40 +1,9 @@
-#include<stdio.h>
-int main(){
-  int num1,num2,num3;
-  printf("\nEnter three numbers");
-  scanf("%d%d%d",&num1,&num2,&num3);
-  if (num1>num2){
-     if (num2 >num3){
-            printf("Number1 is the largest");
-     }
-     else if (num3>num2){
-            if (num1<num3) {
-                printf("number3 is largest");
-            }
-     }
-   }             
-   else{
-      if (num2 >num3){
-            printf("Number2 is the largest");
-   }
-      else if (num3>num2){
-            
-            printf("number3 is largest");
-     }
-  }
-  return 0;
-}
-
-
-```
 #include <stdio.h>
 
 int main() {
     int num1, num2, num3;
-
     printf("Enter three numbers: ");
     scanf("%d %d %d", &num1, &num2, &num3);
-
     if (num1 >= num2) {
         if (num1 >= num3) {
             printf("Number1 is the largest\n");
@@ -51,4 +20,3 @@ int main() {
 
     return 0;
 }
-```
