42851d4155d319c993c5292c432365fe18f31065
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-01-29 15:22:18 +0530

    Update largest.c

--- largest.c
+++ largest.c
@@ -24,3 +24,31 @@ int main(){
   }
   return 0;
 }
+
+
+```
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if (num1 >= num2) {
+        if (num1 >= num3) {
+            printf("Number1 is the largest\n");
+        } else {
+            printf("Number3 is the largest\n");
+        }
+    } else {
+        if (num2 >= num3) {
+            printf("Number2 is the largest\n");
+        } else {
+            printf("Number3 is the largest\n");
+        }
+    }
+
+    return 0;
+}
+```
