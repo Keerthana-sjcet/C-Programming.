df78101a53322e2e43bed36debb3bc846ef25df8
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-01-29 16:12:01 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int number,factorial=1;
+  printf("enter number");
+  scanf("%d",&number);
+  while (number>0){
+    factorial =factorial*number;
+    number=number-1;
+  }
+  printf("%d",factorial);
+  return 0;
+}   
