34b5e648ab225090b33558e674e39f9c21fc6852
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-02-05 16:20:19 +0530

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int i,j, n;
+    printf("Enter number of elements in the array: ");
+    scanf("%d",&n);
+    int array[n];
+    printf("Enter elements of the array %d:",n);
+    for (i=0;i<n;i++){
+        scanf("%d",&array[i]);
+    }
+    for (i=0;i<n-1;i++){
+      for (j=0;j<n-i;j++){
+         
+       if(array[j]>array[j+1]){
+         int temp=array[j];
+         array[j]=array[j+1];
+         array[j+1]=temp;
+       }
+     }
+   }
+   printf("sorted array is\t");
+   for (i=0;i<n;i++){
+      printf("%d \t",array[i]);
+   }
+   return 0;
+}
