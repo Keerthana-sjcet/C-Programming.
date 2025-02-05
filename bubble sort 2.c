2de6f39e879a120acd43ce10c8ea365f73a41774
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-02-05 20:17:53 +0530

    bubble sort 2.c

--- bubble sort 2.c
+++ bubble sort 2.c
@@ -2,22 +2,15 @@
 
 int main() {
     int i, j, n;
-
-    // Taking array size input
     printf("Enter number of elements in the array: ");
     scanf("%d", &n);
-
     int array[n];
-
-    // Taking array elements input
     printf("Enter elements of the array: ");
     for (i = 0; i < n; i++) {
         scanf("%d", &array[i]);
     }
-
-    // Bubble Sort Algorithm
     for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {  // Fixed condition here
+        for (j = 0; j < n - i - 1; j++) {  
             if (array[j] > array[j + 1]) {
                 int temp = array[j];
                 array[j] = array[j + 1];
@@ -25,8 +18,6 @@ int main() {
             }
         }
     }
-
-    // Printing sorted array
     printf("Sorted array is: ");
     for (i = 0; i < n; i++) {
         printf("%d ", array[i]);
