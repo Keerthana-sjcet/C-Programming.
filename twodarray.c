13ef4c944ca74a991ba4fbf634dbb6004e562b12
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-02-07 10:50:35 +0530

    Create twodarray.c

---
+++ twodarray.c
@@ -0,0 +1,29 @@
+/*
+Name: Keerthana 
+Date: 07-02-2025
+Purpose: Program to find sum of diagonal elements.
+*/
+
+#include<stdio.h>
+int main(){
+    int i,j ,sum=0,row,column;
+    printf("Enter no of rows: ");
+    scanf("%d",&row);
+    printf("Enter no of columns: ");
+    scanf("%d",&column);
+    int matrix[row][column];
+    printf("Enter elements:\n");
+    for(i=0;i<row;i++){
+        for (j=0;j<column;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+   printf("The matrix is\n");
+   for (i=0;i<row;i++){
+      for (j=0;j<column;j++){
+          printf("%d\t",matrix[i][j]);
+      }
+      printf("\n");
+  }
+  return 0;
+}
