9c220f9a475b3dd1bfc2ede005517420981e44bf
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-02-17 06:35:25 +0530

    strong.c

--- strong.c
+++ strong.c
@@ -3,35 +3,33 @@ strong number is a number in which the sum of the factorial of the digits is
 equal to the number itself.
 Eg:- 145=1!+4!+5!=1+24+120=145
 */
-
 #include <stdio.h>
 
-int main()
-{
-    int i, originalNum, num, lastDigit, sum,factorial=1;
-    long fact;
+int main() {
+    int i, originalNum, num, lastDigit, sum = 0;
+    long factorial;
+
     printf("Enter any number to check Strong number: ");
     scanf("%d", &num);
+
     originalNum = num;
-    sum = 0;
-    while(num > 0)
-    {
+
+    while (num > 0) {
         lastDigit = num % 10;
-        while (num>0){
-        factorial =factorial*num;
-        num=num-1;
-        sum = sum + fact;
+        factorial = 1;
+        for (i = 1; i <= lastDigit; i++) {
+            factorial = factorial * i;
+        }
+        sum = sum + factorial;
         num = num / 10;
     }
 
-    if(sum == originalNum)
-    {
+    if (sum == originalNum) {
         printf("%d is STRONG NUMBER", originalNum);
-    }
-    else
-    {
+    } else {
         printf("%d is NOT STRONG NUMBER", originalNum);
     }
 
     return 0;
 }
+
