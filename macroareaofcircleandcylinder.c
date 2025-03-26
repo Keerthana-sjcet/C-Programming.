95b72b3c3112af7301f15ef8a58b7f911995533d
Keerthana-sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-03-26 15:05:09 +0530

    macro_area.c

--- macroareaofcircleandcylinder.c
+++ macroareaofcircleandcylinder.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #define Pi 3.14159
+#define circle Pi*r*r
+#define cylinder 2*Pi*r*(r+h)
 int main(){
   float r,h;
   printf("Enter radius");
   scanf("%f",&r);
   printf("Enter height");
   scanf("%f",&h);
-  float areaofcircle=Pi*r*r;
-  printf("Area of circle is %f",areaofcircle);
-  float areaofcylinder=2*Pi*r*(r+h);
-  printf("\nArea of cylinder is %f",areaofcylinder);
+  printf("Area of circle is %f",circle);
+  printf("\nArea of cylinder is %f",cylinder);
   return 0;
 }
   
